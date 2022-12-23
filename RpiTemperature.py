06f6761f305498b5df52907e5d3a23592f7026f3
TimD1981 <43892241+TimD1981@users.noreply.github.com>
2022-12-23 11:27:35 +0100

    Update RpiTemperature.py

--- RpiTemperature.py
+++ RpiTemperature.py
@@ -17,7 +17,7 @@ from pprint import pprint
 
 # our own packages
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-modem'))
-from vedbus import VeDbusService, VeDbusItemExport, VeDbusItemImport 
+from vedbus import VeDbusService, VeDbusItemExport, VeDbusItemImport
 from settingsdevice import SettingsDevice  # available in the velib_python repository
 
 dbusservice = None
@@ -26,7 +26,7 @@ def update():
      update_rpi()
      return True
 
-#   update Pi CPU temperature 
+#   update Pi CPU temperature
 def update_rpi():
     if not os.path.exists('/sys/devices/virtual/thermal/thermal_zone0/temp'):
         if dbus_cpu_service['/Connected'] != 0:
@@ -39,10 +39,81 @@ def update_rpi():
         fd  = open('/sys/devices/virtual/thermal/thermal_zone0/temp','r')
         value = float(fd.read())
         value = round(value / 1000.0, 1)
-        dbus_cpu_service['/Temperature'] = value 
+        dbus_cpu_service['/Temperature'] = value
         fd.close
 
-#  No setting interface for Rpi CPU temperature
+
+# =========================== Start of settings interface ================
+#  The settings interface handles the persistent storage of changes to settings
+#  This should probably be created as a new class extension to the settingDevice object
+#  The complexity is because this python service handles temperature and humidity
+#  Data for about 6 different service paths so we need different dBusObjects for each device
+#
+newSettings = {}     # Used to gather new settings to create/check as each dBus object is created
+settingObjects = {}  # Used to identify the dBus object and path for each setting
+                     # settingsObjects = {setting: [path,object],}
+                     # each setting is the complete string e.g. /Settings/Temperature/4/Scale
+
+settingDefaults = {'/Offset': [0, -10, 10],
+                   '/Scale'  : [1.0, -5, 5],
+                   '/TemperatureType'   : [2, 0, 3],
+                   '/CustomName'        : ['', 0, 0]}
+
+# Values changed in the GUI need to be updated in the settings
+# Without this changes made through the GUI change the dBusObject but not the persistent setting
+# (as tested in venus OS 2.54 August 2020)
+def handle_changed_value(setting, path, value):
+    global settings
+    print("some value changed")
+    # The callback to the handle value changes has been modified by using an anonymouse function (lambda)
+    # the callback is declared each time a path is added see example here
+    # self.add_path(path, 0, writeable=True, onchangecallback = lambda x,y: handle_changed_value(setting,x,y) )
+    logging.info(" ".join(("Storing change to setting", setting+path, str(value) )) )
+    settings[setting+path] = value
+    return True
+
+# Changes made to settings need to be reflected in the GUI and in the running service
+def handle_changed_setting(setting, oldvalue, newvalue):
+    logging.info('Setting changed, setting: %s, old: %s, new: %s' % (setting, oldvalue, newvalue))
+    [path, object] = settingObjects[setting]
+    object[path] = newvalue
+    return True
+
+# Add setting is called each time a new service path is created that needs a persistent setting
+# If the setting already exists the existing recored is unchanged
+# If the setting does not exist it is created when the serviceDevice object is created
+def addSetting(base, path, dBusObject):
+    global settingObjects
+    global newSettings
+    global settingDefaults
+    setting = base + path
+    logging.info(" ".join(("Add setting", setting, str(settingDefaults[path]) )) )
+    settingObjects[setting] = [path, dBusObject]             # Record the dBus Object and path for this setting
+    newSettings[setting] = [setting] + settingDefaults[path] # Add the setting to the list to be created
+
+# initSettings is called when all the required settings have been added
+def initSettings(newSettings):
+    global settings
+
+#   settingsDevice is the library class that handles the reading and setting of persistent settings
+    settings = SettingsDevice(
+        bus=dbus.SystemBus() if (platform.machine() == 'armv7l') else dbus.SessionBus(),
+        supportedSettings = newSettings,
+        eventCallback     = handle_changed_setting)
+
+# readSettings is called after init settings to read all the stored settings and
+# set the initial values of each of the service object paths
+# Note you can not read or set a setting if it has not be included in the newSettings
+#      list passed to create the new settingsDevice class object
+
+def readSettings(list):
+    global settings
+    logging.info("reading")
+    for setting in list:
+        [path, object] = list[setting]
+        logging.info(" ".join(("Retreived setting", setting, path, str(settings[setting]))))
+        object[path] = settings[setting]
+
 # =========================== end of settings interface ======================
 
 class SystemBus(dbus.bus.BusConnection):
@@ -72,7 +143,7 @@ def getrevision():
 
   return myrevision
 
-# Argument parsing removed from source as never used 
+# Argument parsing removed from source as never used
 class args: pass
 args.debug = False
 #args.debug = True
@@ -88,7 +159,7 @@ DBusGMainLoop(set_as_default=True)
 
 def new_service(base, type, physical, logical, id, instance, settingId = False):
     self =  VeDbusService("{}.{}.{}{:02d}".format(base, type, physical,  id), dbusconnection())
-    # physical is the physical connection 
+    # physical is the physical connection
     # logical is the logical connection to allign with the numbering of the console display
     # Create the management objects, as specified in the ccgx dbus-api document
     self.add_path('/Mgmt/ProcessName', __file__)
@@ -104,10 +175,18 @@ def new_service(base, type, physical, logical, id, instance, settingId = False):
     self.add_path('/Connected', 0)  # Mark devices as disconnected until they are confirmed
 
     # Create device type specific objects set values to empty until connected
+    if settingId :
+        setting = "/Settings/" + type.capitalize() + "/" + str(settingId)
+    else:
+        print("no setting required")
+        setting = ""
     if type == 'temperature':
         self.add_path('/Temperature', [])
-       	self.add_path('/Status', 0)
-       	self.add_path('/TemperatureType', 2 )
+        self.add_path('/Status', 0)
+        if settingId:
+            addSetting(setting , '/TemperatureType', self)
+            addSetting(setting , '/CustomName', self)
+        self.add_path('/TemperatureType', 2, writeable=True, onchangecallback = lambda x,y: handle_changed_value(setting,x,y) )
         self.add_path('/CustomName', '', writeable=True, onchangecallback = lambda x,y: handle_changed_value(setting,x,y) )
         self.add_path('/Function', 1, writeable=True )
 
@@ -121,11 +200,16 @@ base = 'com.victronenergy'
 # Items marked with a (*) are included in the service name
 #
 
-dbus_cpu_service   = new_service(base, 'temperature', 'Rpi-cpu',  'Raspberry Pi OS',  6, 29)
+dbus_cpu_service   = new_service(base, 'temperature', 'Rpi-cpu',  'Raspberry Pi OS',  6, 29,6)
 
 # Tidy up custom or missing items
 dbus_cpu_service['/ProductName']     = 'Raspberry Pi'
 dbus_cpu_service['/CustomName']     = 'CPU Temperature'
+
+# Persistent settings obejects in settingsDevice will not exist before this is executed
+initSettings(newSettings)
+# Do something to read the saved settings and apply them to the objects
+readSettings(settingObjects)
 
 # Do a first update so that all the readings appear.
 update()
